e5176b740e1974fadd269df9b76ec6f6300ff62a
saxvaze <saxvaze@yahoo.com>
2015-04-16 00:17:31 +0400

    Registration Design Progress, glyphicons

--- NS.MongoTransaction.Common/Entities/User.cs
+++ NS.MongoTransaction.Common/Entities/User.cs
@@ -13,19 +13,51 @@ namespace NS.MongoTransaction.Common.Entities
         [BsonId]
         public int UserId { get; set; }
 
+        [BsonElement("username")]
         [BsonRepresentation(BsonType.String)]
         public string Username { get; set; }
 
+        [BsonElement("password")]
         [BsonRepresentation(BsonType.String)]
         public string Password { get; set; }
 
+        [BsonElement("firstname")]
         [BsonRepresentation(BsonType.String)]
         public string FirstName { get; set; }
 
+        [BsonElement("lastname")]
         [BsonRepresentation(BsonType.String)]
         public string LastName { get; set; }
 
+        [BsonElement("personalNumber")]
+        [BsonRepresentation(BsonType.String)]
+        public string PersonalNumber { get; set; }
+
+        [BsonElement("birthDate")]
         [BsonRepresentation(BsonType.DateTime)]
-        public string BirthDate { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        [BsonRepresentation(BsonType.String)]
+        [BsonElement("info")]
+        public UserInfo UserInfo { get; set; }
+    }
+
+    [BsonIgnoreExtraElements]
+    public class UserInfo
+    {
+        [BsonIgnoreIfNull]
+        [BsonElement("mobile")]
+        [BsonRepresentation(BsonType.String)]
+        public string Mobile { get; set; }
+
+        [BsonIgnoreIfNull]
+        [BsonElement("email")]
+        [BsonRepresentation(BsonType.String)]
+        public string EMail { get; set; }
+
+        [BsonIgnoreIfNull]
+        [BsonElement("address")]
+        [BsonRepresentation(BsonType.String)]
+        public string Address { get; set; }
     }
 }
--- NS.MongoTransaction.WebFrontEnd/Controllers/AccountController.cs
+++ NS.MongoTransaction.WebFrontEnd/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NS.MongoTransaction.Common.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,12 @@ namespace NS.MongoTransaction.WebFrontEnd.Controllers
         public ActionResult Register()
         {
             return View(); 
+        }
+
+        [HttpPost]
+        public ActionResult Register(User model)
+        {
+            return View();
         }
     }
 }
